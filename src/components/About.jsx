522293ef4d25e18b9f75f1abea633f7ef57fa987
Napolitano <127800985+NapoelonBonaparty@users.noreply.github.com>
2025-09-04 22:22:01 -0600

    Create About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>Acerca de</h1>
+      <p>Este proyecto es parte de la actividad integradora de SCRUM. 👌👌👌</p>
+    </div>
+  );
+};
+
+export default About;
