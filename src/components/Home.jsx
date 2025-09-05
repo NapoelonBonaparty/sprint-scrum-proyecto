dec9e5d618d2d16f662ebadd8b824121a2965355
Napolitano <127800985+NapoelonBonaparty@users.noreply.github.com>
2025-09-04 22:21:15 -0600

    Create Home.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Paina  de Inicio</h1>
+      <p>Bienvenido al proyecto SCRUM en React!!!!!!!!!!!! 💀😭😭😭😭😭.</p>
+    </div>
+  );
+};
+
+export default Home;
